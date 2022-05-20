5bd527a0a42b81c61238df13547dd59734cb6f00
dipa <dipa.smvs@gmail.com>
2022-05-20 10:09:11 +0100

    new added

---
+++ EmployeeDetails.java
@@ -0,0 +1,447 @@
+package Practice;
+
+public class EmployeeDetails {
+    // Create a Variable
+    int id;
+    String Name,Department,Email;
+    int Salary;
+    public static void main(String[] args) {
+        EmployeeDetails Shell = new EmployeeDetails(); //Creating Object of Employee Details Class
+        //Given Value to the Properties
+        Shell.id = (1);
+        Shell.Name = ("John");
+        Shell.Salary = (27000);
+        Shell.Department = ("Service");
+        Shell.Email = ("john34w@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+
+
+//
+
+        //Given Value to the Properties
+        Shell.id = (2);
+        Shell.Name = ("Ella");
+        Shell.Salary = (30000);
+        Shell.Department = ("Finance");
+        Shell.Email = ("Ella123@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (3);
+        Shell.Name = ("Sam");
+        Shell.Salary = (70000);
+        Shell.Department = ("IT");
+        Shell.Email = ("sam90@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (4);
+        Shell.Name = ("Inaya");
+        Shell.Salary = (33000);
+        Shell.Department = ("HR");
+        Shell.Email = ("ina23@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (5);
+        Shell.Name = ("Kethlin");
+        Shell.Salary = (30000);
+        Shell.Department = ("Finance");
+        Shell.Email = ("keth12@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (6);
+        Shell.Name = ("Bella");
+        Shell.Salary = (27000);
+        Shell.Department = ("Service");
+        Shell.Email = ("Bella23@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (7);
+        Shell.Name = ("Meera");
+        Shell.Salary = (39000);
+        Shell.Department = ("Safety");
+        Shell.Email = ("Meera3@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (8);
+        Shell.Name = ("Prince");
+        Shell.Salary = (22000);
+        Shell.Department = ("Marketing");
+        Shell.Email = ("prince56@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (9);
+        Shell.Name = ("Raff");
+        Shell.Salary = (21000);
+        Shell.Department = ("Operation");
+        Shell.Email = ("raff34@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (10);
+        Shell.Name = ("Krishtina");
+        Shell.Salary = (29000);
+        Shell.Department = ("Purchase");
+        Shell.Email = ("Krish22@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+
+        //Given Value to the Properties
+        Shell.id = (11);
+        Shell.Name = ("Sakshi");
+        Shell.Salary = (27000);
+        Shell.Department = ("Service");
+        Shell.Email = ("sakshi44@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+
+
+//
+
+        //Given Value to the Properties
+        Shell.id = (12);
+        Shell.Name = ("Badri");
+        Shell.Salary = (30000);
+        Shell.Department = ("Finance");
+        Shell.Email = ("badri23@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (13);
+        Shell.Name = ("Shiva");
+        Shell.Salary = (70000);
+        Shell.Department = ("IT");
+        Shell.Email = ("shiva@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (14);
+        Shell.Name = ("Merry");
+        Shell.Salary = (33000);
+        Shell.Department = ("HR");
+        Shell.Email = ("merry3@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (15);
+        Shell.Name = ("Ayush");
+        Shell.Salary = (30000);
+        Shell.Department = ("Finance");
+        Shell.Email = ("ayu@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (16);
+        Shell.Name = ("Moksh");
+        Shell.Salary = (27000);
+        Shell.Department = ("Service");
+        Shell.Email = ("moks3@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (17);
+        Shell.Name = ("Joe");
+        Shell.Salary = (39000);
+        Shell.Department = ("Safety");
+        Shell.Email = ("joe5@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (18);
+        Shell.Name = ("Sharan");
+        Shell.Salary = (22000);
+        Shell.Department = ("Marketing");
+        Shell.Email = ("shar6@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (19);
+        Shell.Name = ("Rocky");
+        Shell.Salary = (21000);
+        Shell.Department = ("Operation");
+        Shell.Email = ("rocky9@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (20);
+        Shell.Name = ("Amando");
+        Shell.Salary = (34000);
+        Shell.Department = ("Purchase");
+        Shell.Email = ("ama82@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (21);
+        Shell.Name = ("Jishan");
+        Shell.Salary = (19000);
+        Shell.Department = ("Sales");
+        Shell.Email = ("jish34@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (22);
+        Shell.Name = ("Diona");
+        Shell.Salary = (22000);
+        Shell.Department = ("Operation");
+        Shell.Email = ("dio2@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+
+ //
+        //Given Value to the Properties
+        Shell.id = (23);
+        Shell.Name = ("Helly");
+        Shell.Salary = (34300);
+        Shell.Department = ("R&D");
+        Shell.Email = ("helly3@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+
+//
+        //Given Value to the Properties
+        Shell.id = (24);
+        Shell.Name = ("Shrisha");
+        Shell.Salary = (25000);
+        Shell.Department = ("Purchase");
+        Shell.Email = ("shris@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+//
+        //Given Value to the Properties
+        Shell.id = (25);
+        Shell.Name = ("Gnyani");
+        Shell.Salary = (20000);
+        Shell.Department = ("Media");
+        Shell.Email = ("gnyani12@gmail.com");
+
+        //Print Employee Details
+        System.out.println ("*********");
+        System.out.println("SHELL UK");
+        System.out.println ("*********");
+        System.out.println("id = " +Shell.id);
+        System.out.println("Name = " +Shell.Name);
+        System.out.println("Salary = " +Shell.Salary);
+        System.out.println("Department = " +Shell.Department);
+        System.out.println("Email = " +Shell.Email);
+
+    }
+}
+
+
+
